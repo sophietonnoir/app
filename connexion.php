7b0bc196cff5be1d3affa7775f12eb6f9dc95e71
SophieTonnoir <laptitesophie78@hotmail.fr>
2014-11-16 16:05:53 +0100

    connexion.php ajouté

---
+++ connexion.php
@@ -0,0 +1,78 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="K2K.css" />
+		<link rel="stylesheet" href="connexion.css" />
+		<title>Key To Key - Connexion</title>
+		<link rel="icon" type="image/gif" href="Img/icone.gif" />
+	</head>
+	
+	<?php 
+	
+	
+	$bdd = new PDO('mysql:host=localhost;dbname=keydb','root', '');
+
+
+$sql = $bdd->prepare('SELECT * FROM users WHERE mail= :mail AND password= :pass');
+
+	if(isset($_POST['mail']))	
+	{
+$sql->execute(array(
+	'mail'=> $_POST['mail'],
+	'pass'=> $_POST['password']));
+	$auth=$sql->fetch();
+	}
+		
+	if (isset($auth['id'])and isset($_POST['mail']))
+	{
+		$_SESSION['id']=$auth['id'];
+		$_SESSION['pseudo']=$auth['pseudo'];
+		$_SESSION['admin']=$auth['admin'];
+		$_SESSION['nmaison']=$auth['nmaison'];
+		$_SESSION['age']=$auth['age'];
+		$_SESSION['datei']=$auth['dateinscription'];
+		$_SESSION['codep']=$auth['codepostal'];
+		$_SESSION['pays']=$auth['pays'];
+		$_SESSION['adresse']=$auth['adresse'];
+		$_SESSION['prenom']=$auth['prenom'];
+		$_SESSION['nom']=$auth['nom'];
+		unset($auth);
+	
+	include ("header.php");
+	echo "<p>vous etes identifié</p>";}
+	
+
+	else
+	
+	{
+	include ("header.php");
+	  
+		if(isset($_POST['mail']))
+		{echo"<p id=passwordw>mauvais mot de passe</p>";}
+	
+	echo "
+		<fieldset>
+		<legend> Connexion: </legend>
+		<form method=\"post\" action=\"connexion.php\" />
+				<label for=\"mail\">Adresse mail:</label>
+			<input type=\"mail\" name=\"mail\" />
+				<label for=\"password\">Mot de passe:</label>
+			<input type=\"password\" name=\"password\"/>
+				<label for=\"box\" class=\"inline\">Rester connecté:</label>
+			<input type=\"checkbox\" name=\"box\" id=\"box\"/>
+			
+			<input type=\"submit\" name=\"valider\" value=\"Connexion!\" />
+		</form>
+		</fieldset>"
+		;
+			
+			}
+	include("footer.php");
+			
+			
+	?>
+	</html>
