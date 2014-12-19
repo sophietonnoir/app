1e7e4d376ec603928996059493dc292a86af5e06
yannad <jledorh@icloud.com>
2014-12-19 16:28:09 +0100

    formaulaireauto 0.0.2

--- formaulaireauto.php
+++ formaulaireauto.php
@@ -1,3 +1,9 @@
 <?php Session_start();
 include("Bdd.php");
-$sql=$bdd->prepare
+$sql=$bdd->query("SELECT * FROM keydb.criterelogement");
+$sql2=$bdd->query("SELECT * FROM keydb.criterelinklog");
+$sql=$sql->fetch();
+$sql2=$sql2->fetch();
+
+
+
