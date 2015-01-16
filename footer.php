42b5095376ec743ab826d35815e8c3847fb7c688
Maxmax94 <maximilien.delapille@gmail.com>
2015-01-16 14:32:40 +0100

    footer avec confidentialité

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 					<li><a href="@">Aide</a></li>
 					<li><a href="@">Règles</a></li>
 					<li><a href="conditionsgenerales.php">Conditions générales</a></li>
-					<li><a href="@">Confidentialité</a></li>
+					<li><a href="confidentialite.php">Confidentialité</a></li>
 				</ul>
 				<p id="texte_footer">Le site Key To Key est un site à but non-lucratif ayant pour objectif de mettre en relation les particuliers possédant un logement pour leur permettre de s'échanger le temps des vacances leur habitations.<br>
 				<a href=inscription.php >Inscrivez-vous pour vous aussi participer à cette aventure.</a></br>
