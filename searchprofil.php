59d1e7b73fe01d8d685f63f4214769cac661758d
Maxmax94 <maximilien.delapille@gmail.com>
2015-01-19 18:06:21 +0100

    css search result

--- searchprofil.php
+++ searchprofil.php
@@ -1,8 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="K2K.css" />
+		<title>Key To Key - Backoffice</title>
+		<link rel="icon" type="image/gif" href="Img/icone.gif" />
+		<link rel="stylesheet" href="connexion.css" />
+	</head>
+
+	<body>
+<?php
+include("header.php"); ?>
 
 <?php
-include("modelsearchprof.php");
+include("modelsearchprof.php"); ?>
 	
-	
+	<div id="searchprofil">
+		<?php
 if (!isset($auth['id']))
 {
 echo ("ce pseudo n'existe pas");
@@ -11,4 +26,7 @@ else
 {
 include("vueactionprofil.php");
 }
-?>
+?> 
+<?php include("footer.php"); ?>
+</body>
+</html>
